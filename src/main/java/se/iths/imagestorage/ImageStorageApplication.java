9c2fb5f73a3660043e6150505747488983bf1265
FelixJacobsen <felix.jacobsen@iths.se>
2022-05-04 15:05:10 +0200

    Initial commit

---
+++ src/main/java/se/iths/imagestorage/ImageStorageApplication.java
@@ -0,0 +1,13 @@
+package se.iths.imagestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/imagestorage/ImageStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.imagestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
